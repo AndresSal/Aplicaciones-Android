cf3ae6447667d948c1c8f9b69898550347196a7e
AndresSal <andres.salazar01@epn.edu.ec>
2018-02-04 13:24:27 -0500

    Aplicacion N° 12 Aplicaciones Android - Consumo de servicios web 01

---
+++ App12_ConsumoServiciosW01/app/src/main/java/com/example/user/app12/ConsultaContactosActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.app12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultaContactosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_contactos);
+    }
+}
---
+++ App12_ConsumoServiciosW01/app/src/main/java/com/example/user/app12/ResultadosConsultaActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.app12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultadosConsultaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados_consulta);
+    }
+}
